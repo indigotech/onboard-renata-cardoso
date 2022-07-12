34165daa2d78d84d509f6a8e477db1501f3f3a29
renataalbq <renataalbq1@gmail.com>
2022-07-12 15:44:57 -0300

    setup and hello world

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.highlight}>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: '700',
+    fontSize: 24,
+  },
+});
+
+export default App;
